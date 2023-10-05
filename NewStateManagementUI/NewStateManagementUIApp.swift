897c1503740e8ba9ed9b828423ccf431068c775f
Onur Bulut <10urbulut@gmail.com>
2023-10-05 08:09:26 +0300

    first commit

---
+++ NewStateManagementUI/NewStateManagementUIApp.swift
@@ -0,0 +1,20 @@
+//
+//  NewStateManagementUIApp.swift
+//  NewStateManagementUI
+//
+//  Created by Onur Bulut on 4.10.2023.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct NewStateManagementUIApp: App {
+
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
